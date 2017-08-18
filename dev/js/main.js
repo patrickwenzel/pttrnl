7ad7628a232562d19965ced8e28b5d5b79fc2072
Patrick Wenzel <patrickwenzel@Patricks-MacBook-Pro.local>
2017-08-18 16:01:03 -0400

    clean up of javascriptother minor changes

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,139 +1,181 @@
-(function() {
-  OffCanvas();
-  OffCanvasNav();
-  Code();
-  scrollStart();
-  ScrollLoc();
-})();
-
 // off canvas menu
-function OffCanvas() {
-  var over = document.getElementById("wrapper");
-  var menu = document.getElementsByClassName("menu-button")[0];
-  menu.onclick = function(e) {
-    this.classList.toggle('active');
-    over.classList.toggle("wrp--ovr");
-    e.stopPropagation();
+var o,
+OffCanvas = {
+
+  settings: {
+    over: document.getElementById("wrapper"),
+    menu: document.getElementsByClassName("menu-button")[0]
+  },
+
+  init: function() {
+    o = this.settings;
+    this.showMenu();
+    this.closeWrap();
+  },
+
+  showMenu: function() {
+    o.menu.onclick = function(e) {
+      this.classList.toggle('active');
+      o.over.classList.toggle("wrp--ovr");
+      e.stopPropagation();
+    }
+  },
+
+  closeWrap: function() {
+    o.over.onclick = function(e) {
+      this.classList.remove("wrp--ovr");
+      o.menu.classList.remove('active');
+      e.stopPropagation();
+    }
   }
-  over.onclick = function(e) {
-    this.classList.remove("wrp--ovr");
-    menu.classList.remove('active');
-    e.stopPropagation();
-  }
 
-}
+};
 
-// scrollto pattern
-function scrollTo(element, to, duration) {
-    if (duration <= 0) return;
-    var difference = to - (element.scrollTop + 120);
-    var perTick = difference / duration * 10;
-    setTimeout(function() {
-      element.scrollTop = element.scrollTop + perTick;
-      if (element.scrollTop === to) return;
-      scrollTo(element, to, duration - 10);
-    }, 0);
-    return false;
-}
+var s,
+Scroll = {
 
-// scrollto on start
-function scrollStart() {
-  if(window.location.hash) {
+  settings: {
+  },
+
+  init: function() {
+    s = this.settings;
+    this.scrollToo();
+  },
+
+  scrollToo: function() {
+
+    function goscroll(element, to, duration) {
+      if(duration <= 0) return;
+      difference = to - (element.scrollTop + 120);
+      tick = difference / duration * 10;
+      setTimeout(function() {
+        element.scrollTop = element.scrollTop + tick;
+        if (element.scrollTop == to) return;
+        goscroll(element, to, duration - 10);
+      }, 0);
+      return false;
+    }
+
+    // hash scroll variables
     var loc = window.location.hash;
     var clean = loc.replace("#", "");
-    var goTo = document.getElementById("pttr-" + clean);
-    var pos = goTo.offsetTop;
-    window.onload = function(e) {
-      if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-        /* fix for firefox issue */
-        scrollTo(document.documentElement, pos, 200);
-      } else {
-        /* for everything else */
-        scrollTo(document.body, pos, 200);
-      }
-      e.stopPropagation();
+    if(loc) {
+      var goto = document.getElementById("pttr-" + clean);
+      var pos = goto.offsetTop;
+    } else {
+      var goto;
+      var pos;
     }
-  }
-}
 
-// off canvas nav
-function OffCanvasNav() {
-  var offitem = document.querySelectorAll(".side-nav ul > li");
-  var offtotal = offitem.length;
-  for (var y = 0; y < offtotal; y++) {
-    var subtotal = offitem[y].getElementsByTagName('ul').length;
-    if (subtotal > 0) {
-      offitem[y].classList.add("children");
-    }
-    offitem[y].onclick = function(e) {
-      e.preventDefault();
-      if (e.target == e.currentTarget) {
-        if (this.children.length > 0) {
-          this.classList.toggle("active");
-          this.children[1].classList.toggle("open");
-          e.stopPropagation();
+    // onload scroll to
+    if(loc) {
+      window.onload = function(e) {
+        if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+          /* fix for firefox issue */
+          goscroll(document.documentElement, pos, 200);
+        } else {
+          /* for everything else */
+          goscroll(document.body, pos, 200);
         }
-      }
-      // scroll to hash
-      var link = e.target.getAttribute("href");
-      var clean = link.replace("#", "");
-      var goTo = document.getElementById("pttr-" + clean);
-      var pos = goTo.offsetTop;
-
-      if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-        /* fix for firefox issue */
-        scrollTo(document.documentElement, pos, 200);
-      } else {
-        /* for everything else */
-        scrollTo(document.body, pos, 200);
+        e.stopPropagation();
       }
     }
-  }
-}
 
-function ScrollLoc() {
-  var location = document.querySelectorAll('[id^="pttr-"]');
-  var loclength = location.length;
-  var links = document.querySelectorAll(".side-nav li > a");
-  var timer;
+    // off canvas
+    var offitem = document.querySelectorAll(".side-nav ul > li");
+    var offtotal = offitem.length;
 
-  window.onscroll = function(e) {
-    if(timer) {
-      window.clearTimeout(timer);
-    }
-    // timeout fix for browser lag issue
-    timer = window.setTimeout(function() {
-      for(var x = 0; x < loclength; x++) {
-        var pttr_id = location[x].id.replace('pttr-','');
-        var pttr_top = location[x].offsetTop;
-        // update hash
-        if (pttr_top < window.pageYOffset + 124 && pttr_top + location[x].offsetHeight + 124) {
-          window.location.hash = pttr_id;
-        }
-        // add class on current href
-        if(links[x].getAttribute('href') == window.location.hash) {
-          var current = document.querySelector(".current");
-          if(current !== null) {
-            current.classList.remove("current");
-            current.parentElement.parentNode.classList.remove('open');
-            current.parentElement.parentNode.parentNode.classList.remove('active');
+    for (var y = 0; y < offtotal; y++) {
+      var subtotal = offitem[y].getElementsByTagName('ul').length;
+      if (subtotal > 0) {
+        offitem[y].classList.add("children");
+      }
+      offitem[y].onclick = function(ev) {
+        ev.stopPropagation();
+        if (ev.target == ev.currentTarget) {
+          if (this.children.length > 0) {
+            this.classList.toggle("active");
+            this.children[1].classList.toggle("open");
+            ev.stopPropagation();
           }
-          links[x].classList.add('current');
-          links[x].parentElement.parentNode.classList.add('open');
-          links[x].parentElement.parentNode.parentNode.classList.add('active');
+        }
+        var link = ev.target.getAttribute("href");
+        var clean = link.replace("#", "");
+        var goTo = document.getElementById("pttr-" + clean);
+        var pos = goTo.offsetTop;
+        if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+          goscroll(document.documentElement, pos, 200);
+        } else {
+          goscroll(document.body, pos, 200);
         }
       }
-    }, 100);
-  }
-}
+    }
 
-function Code() {
-  var code_btn = document.getElementsByClassName("code-btn");
-  var ctotal = code_btn.length;
-  for (var i = 0; i < ctotal; i++) {
-    code_btn[i].onclick = function(e) {
-      this.classList.toggle("show");
-      e.stopPropagation();
+    // scroll location
+    var location = document.querySelectorAll('[id^="pttr-"]');
+    var loclength = location.length;
+    var links = document.querySelectorAll(".side-nav li > a");
+    var timer;
+
+    window.onscroll = function(e) {
+      if(timer) {
+        window.clearTimeout(timer);
+      }
+      // timeout fix for browser lag issue
+      timer = window.setTimeout(function() {
+        for(var x = 0; x < loclength; x++) {
+          var pttr_id = location[x].id.replace('pttr-','');
+          var pttr_top = location[x].offsetTop;
+          // update hash
+          if (pttr_top < window.pageYOffset + 124 && pttr_top + location[x].offsetHeight + 124) {
+            window.location.hash = pttr_id;
+          }
+          // add class on current href
+          if(links[x].getAttribute('href') == window.location.hash) {
+            var current = document.querySelector(".current");
+            if(current !== null) {
+              current.classList.remove("current");
+              current.parentElement.parentNode.classList.remove('open');
+              current.parentElement.parentNode.parentNode.classList.remove('active');
+            }
+            links[x].classList.add('current');
+            links[x].parentElement.parentNode.classList.add('open');
+            links[x].parentElement.parentNode.parentNode.classList.add('active');
+          }
+        }
+      }, 100);
+    }
+
+  }
+
+};
+
+var c,
+Code = {
+
+  settings: {
+    btn: document.getElementsByClassName("code-btn"),
+  },
+
+  init: function() {
+    c = this.settings;
+    this.showCode();
+  },
+
+  showCode: function() {
+    total = c.btn.length;
+    for (var i = 0; i < total; i++) {
+      c.btn[i].onclick = function(e) {
+        this.classList.toggle("show");
+        e.stopPropagation();
+      }
     }
   }
-}
+
+};
+
+(function() {
+  OffCanvas.init();
+  Scroll.init();
+  Code.init();
+})();
